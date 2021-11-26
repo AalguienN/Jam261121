64ebbbe210b25db0df7c494df655df83cd677e13
Adrián Camacho <adricamachogarcia@gmail.com>
2021-11-26 13:59:05 +0100

    Inicializa cosas
    Ahora inicializa enemigos, cofres y curaciones

--- JamJuego/Assets/Scripts/SelectorDeMazmorra.cs
+++ JamJuego/Assets/Scripts/SelectorDeMazmorra.cs
@@ -6,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class SelectorDeMazmorra : MonoBehaviour
 {
     [Header("Probabilidades")]
-    //Tocar estos 3 parámetros para cambiar las probabilidades de los mapas;
+    //Tocar estos 3 parámetros para cambiar las probabilidades en los mapas
     public int probSEnemigo = 50;
     public int probSCofre = 25;
     public int probSCuracion = 25;
 
+    [Header("Prefabs")]
+    public GameObject[] enemigos;
+    public GameObject cofre;
     private int probTotales;
+
+    [Header("ObjetoCargado")]
+    private GameObject objetoActivo;
+
 
     public void Start()
     {
@@ -20,22 +27,32 @@ public class SelectorDeMazmorra : MonoBehaviour
     }
    
     public void NuevaSala() {
+        // 
+        if (objetoActivo != null) {
+            Destroy(objetoActivo);
+        }
+        
         int nAleatorio = Random.Range(0, probTotales);
         print (nAleatorio);
         if (nAleatorio < probSEnemigo)
         {
             //Load Enemigo
+            objetoActivo = Instantiate(enemigos[0], new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
+            objetoActivo.name = "Enemigo";
             print("Enemigo");
         }
         else if (nAleatorio < probSCofre + probSEnemigo)
         {
             //Load Cofre
+            objetoActivo = Instantiate(cofre, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
+            objetoActivo.name = "Cofre";
             print("Cofre");
         }
         else
         {
+            // Load Curación
             print("Curacion");
-            //Load Curación
+            
         }
     }
 }
