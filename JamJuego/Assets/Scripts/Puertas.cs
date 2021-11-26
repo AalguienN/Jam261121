05efe542d99974dcac38378b7d66cd8f5d6f5126
Martajm18 <martajm18@gmail.com>
2021-11-26 15:58:42 +0100

    ok

--- JamJuego/Assets/Scripts/Puertas.cs
+++ JamJuego/Assets/Scripts/Puertas.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Puertas : MonoBehaviour
 {
-    public static bool pasar;
     private BoxCollider2D box;
     public GameObject Personaje;
     public GameObject Posicion;
@@ -26,7 +25,6 @@ public class Puertas : MonoBehaviour
             Personaje.transform.position = VectPos;
             box = Posicion.GetComponent<BoxCollider2D>();
             box.enabled = false;
-            pasar = true;
 
         }
     }
