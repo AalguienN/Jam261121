69b601599ece9a3b11663d22bb3467f8b3ac88bf
Adr333n <adr333n@gmail.com>
2021-11-26 12:19:23 +0100

    Codigo David

--- JamJuego/Assets/Scripts/Card.cs
+++ JamJuego/Assets/Scripts/Card.cs
@@ -11,4 +11,16 @@ public class Card : ScriptableObject
 
     public int cost;
     public int damage;
+    public int contador;        //Contador de cartas
+
+    
+    public List<Card> mazo = new List<Card>();     //Numero maximo de cartas disponible (20)
+    public List<Card> mano = new List<Card>();     //Numero maximo de cartas que puede llevar el jugador (4)
+
+
+    public void Cartas(List<Card> c) {
+        
+        contador = c.Count;
+    
+    }
 }
